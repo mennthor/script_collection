c59b537beb265615ecabe5de6f0053359599b2b2
Thorben Menne <mennthor@aol.com>
2020-06-03 13:40:43 +0200

    Added support for batch processing in replace spaces script

--- script_collection/replace_spaces.py
+++ script_collection/replace_spaces.py
@@ -31,33 +31,37 @@ _UNICODE_SPACES = [  # http://jkorpela.fi/chars/spaces.html
 def main():
     """ Entrypoint """
     parser = argparse.ArgumentParser(description="replace_spaces")
-    parser.add_argument("file_path", type=str, help="File to handle.")
+    parser.add_argument("file_path", nargs="+", help="File(s) to handle.")
     parser.add_argument("--char", type=str, default=".",
                         help="Character used to replace spaces. (default '.')")
     parser.add_argument("-c", action="store_true",
-                        help="If given copies the file with the new name.")
+                        help="If given copies the file(s) with the new name.")
     parser.add_argument("-n", action="store_true",
                         help="Dry run, only prints the new name.")
     args = parser.parse_args()
-    file_path = args.file_path
+    file_paths = args.file_path
     replace_char = args.char
     copy = args.c
     dry = args.n
+    if dry:
+        print("Dry mode, would {} modified files like".format(
+            "copy" if copy else "rename"))
 
-    base = os.path.basename(file_path)
-    path = os.path.dirname(file_path)
+    for file_path in file_paths:
+        base = os.path.basename(file_path)
+        path = os.path.dirname(file_path)
 
-    for space in _UNICODE_SPACES:
-        base = base.decode("utf-8").replace(space, replace_char).encode("utf-8")
+        for space in _UNICODE_SPACES:
+            base = base.replace(space, replace_char)
 
-    out_path = os.path.join(path, base)
+        out_path = os.path.join(path, base)
 
-    if copy and not dry:
-        copy_file(file_path, out_path)
-    elif not dry:
-        os.rename(file_path, out_path)
-    else:
-        print(out_path)
+        if copy and not dry:
+            copy_file(file_path, out_path)
+        elif not dry:
+            os.rename(file_path, out_path)
+        else:
+            print("- {}".format(out_path))
 
 
 if __name__ == "__main__":
